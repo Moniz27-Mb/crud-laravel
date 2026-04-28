ff4b2ed26edf66b6216b4e8dab50c973d7aa275e
Moniz <mbembomoniz981@gmail.com>
2026-04-28 10:24:49 +0100

    breeze

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('img/principal.png') }}" alt="Logo" style="border-radius: 50%; object-fit: cover;" {{ $attributes }}>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProdutoController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('produtos.index');
+    return redirect()->route('login');
 });
 
-Route::resource('produtos', ProdutoController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return redirect()->route('produtos.index');
+    })->name('dashboard');
+
+    Route::resource('produtos', ProdutoController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
